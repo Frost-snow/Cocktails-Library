b6cc41d0cb8030739fd2c423cd1251a0641b5472
BucketheadX <kdv90@mail.ru>
2013-03-27 19:42:12 +0700

    queue delete

--- src/com/CocktailsLibrary/Common/Interfaces/IRepository.java
+++ src/com/CocktailsLibrary/Common/Interfaces/IRepository.java
@@ -1,11 +1,11 @@
 package com.CocktailsLibrary.Common.Interfaces;
 
-import java.util.Queue;
+import java.util.List;
 
 // Represents a repository for entity repository.
 public interface IRepository<TId extends Comparable<TId>, TEntity extends IEntity<TId>> {
-    // Defines a query to all items.
-    Queue<TEntity> items();
+    // Defines a list to all items.
+    List<TEntity> listItems();
 
     // Gets an item by specified id.
     TEntity getById(TId id);
--- src/com/CocktailsLibrary/DataAccess/XmlFileRepository.java
+++ src/com/CocktailsLibrary/DataAccess/XmlFileRepository.java
@@ -1,10 +1,11 @@
-package com.CocktailsLibrary.DataAccess;
+/*package com.CocktailsLibrary.DataAccess;
 
 import com.CocktailsLibrary.Common.Interfaces.IEntity;
 import com.CocktailsLibrary.Common.Interfaces.IRepository;
 
 import java.io.File;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 // Represents an XML-File based repository implementation.
@@ -19,6 +20,11 @@ public class XmlFileRepository<TEntity extends IEntity<UUID>> extends IRepositor
 
     // Gets a file extension fro entity file.
     private final String _fileExt = ".entity.xml";
+
+    // Gets entity type name for path.
+    private String getTypeName(){
+        return "";
+    }
 
     // File name format: {RootPath}/{EntityTypeName}/{id:00000000000000000000000000000000}.entity.xml.
     protected String getFileNameForEntity(UUID id){
@@ -43,9 +49,22 @@ public class XmlFileRepository<TEntity extends IEntity<UUID>> extends IRepositor
 
     }
 
+    protected TEntity unpackFile(UUID id){
+
+    }
+
     @Override
-    public Queue items() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public List<TEntity> listItems() {
+        if (!this._directory.exists()){
+            this._directory.mkdirs();
+        }
+
+        File[] files = this._directory.listFiles();
+        List<TEntity>
+        for (int i = 0; i < listItems().size(); i++){
+
+        }
+        return this._directory.listFiles();
     }
 
     @Override
@@ -67,4 +86,4 @@ public class XmlFileRepository<TEntity extends IEntity<UUID>> extends IRepositor
     public void create() {
         //To change body of implemented methods use File | Settings | File Templates.
     }
-}
+}     */
