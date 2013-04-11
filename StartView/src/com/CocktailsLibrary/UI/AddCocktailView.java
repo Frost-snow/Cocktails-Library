2580ef5d9afeb51a83565835e1f525404bed3079
Frost-snow <Frostololo@gmail.com>
2013-04-11 23:26:33 +0700

    addCocktailView + layout

--- StartView/src/com/CocktailsLibrary/UI/AddCocktailView.java
+++ StartView/src/com/CocktailsLibrary/UI/AddCocktailView.java
@@ -1,5 +1,6 @@
 package com.CocktailsLibrary.UI;
 
+import android.R;
 import android.os.Bundle;
 
 import com.CocktailsLibrary.UI.Activities.Base.ActivityBase;
@@ -8,6 +9,7 @@ public class AddCocktailView extends ActivityBase {
 
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		setContentView(com.CocktailsLibrary.R.layout.addcocktail_view);
 		
 	}
 	@Override
